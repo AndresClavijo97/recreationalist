c9cd1dd35ab12b34b8b0d0383d19966aba99208f
Andres Felipe Clavijo <andress.clavijo96@gmail.com>
2019-02-27 12:20:06 -0500

    se cambio en la tabla users la columna phon... de int a string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_142555) do
+ActiveRecord::Schema.define(version: 2019_02_27_165744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_142555) do
     t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
