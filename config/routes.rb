e1150beabd12043f6aa59451957b37671264c538
Andres Felipe Clavijo <andress.clavijo96@gmail.com>
2019-02-27 09:27:39 -0500

    se agrego un campo role a el modelor user ademas de los roles en un objeto enum

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # redireciona a una "landing page" de no estar authenticado 
+  unauthenticated  do
+    root to: 'landing_page#home'
+    get '/about', to: 'landing_page#about'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
